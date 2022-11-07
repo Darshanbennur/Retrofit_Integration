103a46f08126acd5c73a01dd6a45817e7dd4efba
DARSHAN <bennurdarshan@gmail.com>
2022-11-07 23:51:23 +0530

    first commit

---
+++ app/src/main/java/com/example/mvvm_retrofit/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.mvvm_retrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mvvm_retrofit.databinding.ActivityMainBinding
+import retrofit2.HttpException
+import java.io.IOException
+
+const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    private lateinit var todoAdapter : TodoAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupRecyclerView()
+
+        lifecycleScope.launchWhenCreated {
+            binding.progresBar.isVisible = true
+            val response = try{
+                RetrofitInstance.api.getTodos()
+            } catch (E : IOException){
+                Log.e(TAG,"IOException, No Internet")
+                binding.progresBar.isVisible = false
+                return@launchWhenCreated
+            }catch (e : HttpException){
+                Log.e(TAG, "HttpException, unexpected Response")
+                binding.progresBar.isVisible = false
+                return@launchWhenCreated
+            }
+            if (response.isSuccessful && response.body() != null){
+                todoAdapter.todos = response.body()!!
+            }else{
+                Log.e(TAG,"Response not successful")
+            }
+            binding.progresBar.isVisible = false
+        }
+    }
+
+    private fun setupRecyclerView() = binding.recView.apply {
+        todoAdapter = TodoAdapter()
+        adapter = todoAdapter
+        layoutManager = LinearLayoutManager(this@MainActivity)
+    }
+}
---
+++ app/src/main/java/com/example/mvvm_retrofit/Todo.kt
@@ -0,0 +1,8 @@
+package com.example.mvvm_retrofit
+
+data class Todo(
+    val completed: Boolean,
+    val id: Int,
+    val title: String,
+    val userId: Int
+)
